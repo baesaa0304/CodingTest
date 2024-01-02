40182549c1e8ff5b8eb193a09d140ae07677d5cf
PearLine_Zero <122285671+baesunyoung34@users.noreply.github.com>
2024-01-02 17:27:06 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 212 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        for(int i = 0; i < a / 4 ; i++){
+        for(int i = 0; i < (a / 4); i++){
             System.out.print("long ");
         }
         System.out.println("int");
