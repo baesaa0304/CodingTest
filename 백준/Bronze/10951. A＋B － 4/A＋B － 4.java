7353e069239f3c13c41f7ac26bd8035917ccdc88
PearLine_Zero <122285671+baesunyoung34@users.noreply.github.com>
2024-01-04 00:15:48 +0900

    [Bronze V] Title: A+B - 4, Time: 232 ms, Memory: 17988 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a , b , c;
+        while(sc.hasNextInt()) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            c = a+b;
+            System.out.println(c);
+        }
+    }
+}
