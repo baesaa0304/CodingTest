27510e9aa5b249c2faf14ff86e006390e626def7
PearLine_Zero <122285671+baesunyoung34@users.noreply.github.com>
2024-03-14 18:43:37 +0900

    [Bronze V] Title: 문자열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.py
+++ 백준/Bronze/9086. 문자열/문자열.py
@@ -1,14 +1,4 @@
-num = int(input())
-str_list = []  # 문자들 입력받는 배열
-str_answer = []  # 시작문자 끝문자 받는 배열
-
-for _ in range(num):
-    string = input()
-    str_list.append(string)
-
-for i in str_list:
-    start = i[0]  # 시작 문자 추출
-    end = i[-1]  # 끝 문자 추출
-    str_answer.append(start + end)  # 시작과 끝 문자를 합쳐서 저장
-
-print(*str_answer, sep='\n')  # 엔터로 구분하여 출력
+N = int(input())
+for _ in range(N):
+    word = list(input())
+    print(word[0] + word[-1])     
