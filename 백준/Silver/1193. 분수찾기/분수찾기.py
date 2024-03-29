a8f70bd3fc34bb26e6f070053873a25c44996529
PearLine_Zero <122285671+baesaa0304@users.noreply.github.com>
2024-03-29 18:41:19 +0900

    [Silver V] Title: 분수찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,15 +1,15 @@
 n = int(input())
+
 line = 1
 
 while n > line:
     n -= line
     line += 1
-
+    
 if line % 2 == 0:
-    up = n
-    down = line - n + 1
+    a = n
+    b = line-n+1
 else:
-    up = line - n + 1
-    down = n
-
-print(up,'/',down, sep="")
+    a = line-n+1
+    b = n
+print(a, '/', b, sep='')
