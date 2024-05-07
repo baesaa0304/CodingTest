2ebd2b254be35fca53c7c2ff5166f055d7415202
PearLine_Zero <122285671+baesaa0304@users.noreply.github.com>
2024-05-07 15:45:01 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 600 ms, Memory: 294804 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        Queue<Integer>que = new LinkedList<>();
+        
+        for (int i = 1; i <= N; i++) {
+        	que.offer(i);
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        
+        while(que.size() !=  1) {
+        	for(int i = 0; i < M - 1; i++) {
+            	que.offer(que.poll());
+            } 
+        	sb.append(que.poll()).append(", ");
+        } 
+      sb.append(que.poll()).append(">");
+      System.out.println(sb);
+	}
+	
+}
