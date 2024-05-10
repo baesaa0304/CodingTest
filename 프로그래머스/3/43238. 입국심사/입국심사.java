df6d7f9353b7f7098e7676ce8a5976e0522b8a45
PearLine_Zero <122285671+baesaa0304@users.noreply.github.com>
2024-05-10 21:50:15 +0900

    [level 3] Title: 입국심사, Time: 0.06 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-	long answer = Long.MAX_VALUE;;
+	long answer = 0;
     long min_time = times[0];
     long max_time =  (long)times[times.length-1]*(long)n;
         while(min_time <= max_time) {
