b765c778c2f031e579c6bd1e6e27748f2421569e
PearLine_Zero <122285671+baesaa0304@users.noreply.github.com>
2024-05-19 10:01:03 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 520 ms, Memory: 114384 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,50 +1,56 @@
+
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N , M;
-	static int u , v;
-	static int[][] arr;
-	static boolean []visit;
-	static int cnt;
-	static Queue<Integer>que = new LinkedList<>();
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N+1][N+1];
-		visit = new boolean[N+1];
-		for(int i = 0; i < M; i++) {
-			StringTokenizer sts = new StringTokenizer(br.readLine());
-			u = Integer.parseInt(sts.nextToken());
-			v = Integer.parseInt(sts.nextToken());
-			arr[u][v] = arr[v][u] = 1;
-		}
-		for(int i =1; i <= N; i++) {
-			if(!visit[i]) {
-				bfs(i);
-				cnt++;
-			}
-		}
-		System.out.println(cnt);
-		
-	
-	}
+    final static int MAX = 1000 + 10;
+    static boolean graph[][];
+    static boolean visited[];
+    static int N, M;
+    static int answer;
 
-	public static void bfs(int start) {
-		que.offer(start);
-		visit[start] = true;
-		while(!que.isEmpty()) {
-			int temp = que.poll();
-			for(int i = 1; i <=N; i++) {
-				if(arr[temp][i] == 1 && !visit[i]) {
-					que.offer(i);
-					visit[i] = true;
-				}
-			}
-		}
-		
-	}
+    static void dfs(int idx){
+        visited[idx] = true;
+        for (int i = 1; i <= N ; i++) {
+            if(visited[i] == false && graph[idx][i]){
+                dfs(i);
+            }
+        }
+    }
 
+    public static void main(String[] args) throws IOException {
+		    // 입출력 처리
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        // graph에 연결 정보 채우기
+        graph = new boolean[MAX][MAX];
+        visited = new boolean[MAX];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u][v] = true; //양방향 그래프라서 [u][v], [v][u]모두 true로 설정
+            graph[v][u] = true;
+        }
+
+        // dfs(재귀함수 호출)
+        for (int i = 1; i <=N; i++) {
+            if(visited[i] == false) { // 방문하지 않은 정점에서 dfs 호출해 연결요소 탐색
+                dfs(i);
+                answer++; // dfs 호출 하는 횟수가 정답
+            }
+        }
+
+        // 출력
+        bw.write(String.valueOf(answer));
+        
+        bw.close();
+        br.close();
+    }
 }
