4b1bce37e7537981eb53091194dafba1f48e1b65
PearLine_Zero <122285671+baesaa0304@users.noreply.github.com>
2024-07-04 15:37:36 +0900

    [level 2] Title: 방문 길이, Time: 18.37 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,37 +2,47 @@ import java.util.*;
 
 class Solution {
     public int solution(String dirs) {
-        int N = 11;
-        int x = 5, y = 5; // 격자의 중앙에서 시작
+        // 방향과 경계 설정
+        int[] dx = {0, 0, -1, 1}; // 상(U), 하(D), 좌(L), 우(R)
+        int[] dy = {1, -1, 0, 0};
+        Map<Character, Integer> directionMap = new HashMap<>();
+        directionMap.put('U', 0);
+        directionMap.put('D', 1);
+        directionMap.put('L', 2);
+        directionMap.put('R', 3);
+        
         Set<String> visitedPaths = new HashSet<>();
-
+        int x = 0, y = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+        
+        // BFS 실행
         for (char dir : dirs.toCharArray()) {
-            int nx = x, ny = y;
-            if (dir == 'U' && x > 0) {
-                nx--; // 위로 이동
-            }
-            else if (dir == 'D' && x < 10) {
-                nx++; // 아래로 이동
-            }
-                
-            else if (dir == 'L' && y > 0) {
-                ny--; // 왼쪽으로 이동
-            }
-            else if (dir == 'R' && y < 10) {
-                ny++; // 오른쪽으로 이동
-            }
-
-            if (nx != x || ny != y) { // 유효한 이동인 경우
+            int[] current = queue.poll();
+            x = current[0];
+            y = current[1];
+            
+            int move = directionMap.get(dir);
+            int nx = x + dx[move];
+            int ny = y + dy[move];
+            
+            // 경계 체크
+            if (nx >= -5 && nx <= 5 && ny >= -5 && ny <= 5) {
+                // 경로 추가
                 String path1 = x + "," + y + "," + nx + "," + ny;
                 String path2 = nx + "," + ny + "," + x + "," + y;
-                visitedPaths.add(path1); // 경로 추가
-                visitedPaths.add(path2); // 반대 경로도 추가
-
-                x = nx;
-                y = ny;
+                visitedPaths.add(path1);
+                visitedPaths.add(path2);
+                
+                // 큐에 새로운 좌표 추가
+                queue.offer(new int[]{nx, ny});
+            } else {
+                // 경계 벗어난 경우 큐에 다시 추가
+                queue.offer(new int[]{x, y});
             }
         }
-
-        return visitedPaths.size() / 2; // 각 경로가 두 번 카운트되므로 절반으로 나눔
+        
+        // 고유 경로의 수 반환
+        return visitedPaths.size() / 2;
     }
 }
