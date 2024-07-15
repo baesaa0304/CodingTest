fcde3995398426f84fcc2df1fe73104e3ebc3d71
PearLine_Zero <122285671+baesaa0304@users.noreply.github.com>
2024-07-15 15:16:15 +0900

    [level 3] Title: 표 편집, Time: 797.97 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -3,7 +3,7 @@ import java.util.Stack;
 class Solution {
     public String solution(int n, int k, String[] cmd) {
         // 삭제된 행의 인덱스를 저장하는 스택
-        Stack<Integer> remove_order = new Stack<Integer>();
+        Stack<Integer> stack = new Stack<Integer>();
         
         // 현재 표의 크기를 저장하는 변수
         int table_size = n;
@@ -23,7 +23,7 @@ class Solution {
             // "C" 명령어 처리
             else if(c=='C') {
                 // 현재 커서 위치의 행을 삭제하고 스택에 저장
-                remove_order.add(k);
+                stack.add(k);
                 table_size--;
                 
                 // 만약 현재 커서가 마지막 행이었다면 커서를 1 감소시킴
@@ -34,7 +34,7 @@ class Solution {
             // "Z" 명령어 처리
             else if(c=='Z') {
                 // 가장 최근에 삭제된 행의 인덱스를 스택에서 pop
-                if(remove_order.pop()<=k)
+                if(stack.pop()<=k)
                     k++;
                 table_size++;
             }
@@ -44,9 +44,9 @@ class Solution {
         StringBuilder builder = new StringBuilder();
         for(int i=0; i<table_size; i++)
             builder.append("O");
-        while(!remove_order.isEmpty())
-            builder.insert(remove_order.pop().intValue(), "X");
-        String answer=builder.toString();
+        while(!stack.isEmpty())
+            builder.insert(stack.pop().intValue(), "X");
+        String answer = builder.toString();
         return answer;
     }
 }
