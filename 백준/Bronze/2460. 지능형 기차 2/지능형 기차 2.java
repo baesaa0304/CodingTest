1c29bf91da536996f794a23bc2d3f69626294aa8
SunyoungBae <baesaa0304@naver.com>
2024-10-01 20:32:02 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int train[] = new int[10];
+		int people = 0;
+		int result = 0;
+		for(int i = 0; i < 10; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int gets_in = Integer.parseInt(st.nextToken());
+			int gets_out = Integer.parseInt(st.nextToken());
+			people = people - gets_in + gets_out;
+			train[i] = people;
+		}
+		for(int i : train) {
+			if(result < i) {
+				result = i;
+			}
+		}
+		System.out.println(result);
+
+	}
+
+}
