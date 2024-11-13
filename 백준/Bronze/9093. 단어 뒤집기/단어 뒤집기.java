8592ce3f185b065df1e3d2cb2f4c4d071d254a6b
SunyoungBae <baesaa0304@naver.com>
2024-11-13 15:20:16 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 412 ms, Memory: 41508 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,5 +1,3 @@
-
-
 import java.io.*;
 import java.util.*;
 
@@ -7,18 +5,20 @@ public class Main {
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            StringBuilder result = new StringBuilder();
-            while (st.hasMoreTokens()) {
-                String word = st.nextToken();
-                StringBuffer sb = new StringBuffer(word);
-                String reversedStr = sb.reverse().toString();
-                result.append(reversedStr).append(" "); // 각 단어 뒤에 공백 추가
-            }
-            System.out.println(result.toString().trim()); // 마지막 공백 제거 후 출력
-        }
+		 int N = Integer.parseInt(br.readLine());
+		 StringBuilder result = new StringBuilder();
+		 for (int i = 0; i < N; i++) {
+			 StringTokenizer st = new StringTokenizer(br.readLine()); // 한 줄로 읽는
+			 while (st.hasMoreTokens()) { // hasMoreToken 공백을 기준으로 나눔 
+				 String s = st.nextToken();
+				 StringBuilder sb = new StringBuilder(s);
+				 result.append(sb.reverse()).append(" ");
+			 }
+			  result.append("\n");
+		}
+		 System.out.print(result.toString());
+		
+
 	}
 
 }
