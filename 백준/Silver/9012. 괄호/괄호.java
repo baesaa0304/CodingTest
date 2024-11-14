c568c012d7e9683b828eb8cb1a7b3b25a4f620b9
SunyoungBae <baesaa0304@naver.com>
2024-11-14 17:09:24 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,41 +1,41 @@
-
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		String yes = "( )";
-		for(int i = 0; i < N; i++) {
-			String input = br.readLine();
-			if(isValid(input)) {
-				System.out.println("YES");
-			}
-			else {
-				System.out.println("NO");
-			}
-			
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<String> result = new ArrayList<>();
 
-	private static boolean isValid(String s) {
-		Stack<Character> stack = new Stack<>();
-		for(int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			if (c == '(') {
-                stack.push(c);
-            } else if (c == ')') {
-                if (stack.isEmpty()) {
-                    return false;
-                } else {
-                    stack.pop();
+        for (int i = 0; i < N; i++) {
+            String gaho = br.readLine();
+            Stack<Character> stack = new Stack<>();  // 스택을 매번 새로 초기화
+            boolean isValid = true;
+
+            for (Character c : gaho.toCharArray()) {
+                if (c.equals('(')) {
+                    stack.push(c);
+                } else if (c.equals(')')) {
+                    if (stack.isEmpty()) {
+                        isValid = false;  // 스택이 비어있는데 ')'가 나오면 유효하지 않음
+                        break;
+                    } else {
+                        stack.pop();
+                    }
                 }
-            }    
-		}
-		return stack.isEmpty();
-	}
+            }
 
-}
+            // 모든 괄호를 검사한 후 스택이 비어있고 유효하면 YES, 아니면 NO
+            if (isValid && stack.isEmpty()) {
+                result.add("YES");
+            } else {
+                result.add("NO");
+            }
+        }
+
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+} 
