7100a0a78c88589f838ee7ca4b647382f8495c06
SunyoungBae <baesaa0304@naver.com>
2024-11-28 15:56:27 +0900

    [Silver II] Title: 쇠막대기, Time: 160 ms, Memory: 16380 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Character> stack = new Stack<>();
+		int result = 0;
+		String s = br.readLine();
+		for(int i  = 0; i < s.length(); i++) {
+			Character c = s.charAt(i);
+			if(c.equals('(')) {
+				stack.push(c);
+			}
+			else if(c.equals(')')) {
+				stack.pop();
+				if (s.charAt(i - 1) == '(') {
+                    // 레이저일 때는 스택의 개수를 더해줌
+                    result += stack.size();
+                } else {
+                    // 쇠막대기가 끝날 때마다 +1
+                    result++;
+                }
+			}
+		}
+		System.out.println(result);
+
+	}
+
+}
