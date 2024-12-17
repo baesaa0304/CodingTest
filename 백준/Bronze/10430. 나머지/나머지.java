d81445597339e89a4291896790f01842723d3b9e
SunyoungBae <baesaa0304@naver.com>
2024-12-17 14:39:13 +0900

    [Bronze V] Title: 나머지, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,14 +1,18 @@
+import java.io.*;
 import java.util.*;
-public class Main{
-	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
-		int a, b, c;
-		a = sc.nextInt();
-		b = sc.nextInt();
-        c = sc.nextInt();
-		System.out.println((a + b) % c);
-        System.out.println(((a%c) + (b%c)) % c);
-        System.out.println((a * b)% c);
-        System.out.println((a % c) * (b % c)% c);
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		System.out.println((A+B)%C);
+		System.out.println(((A%C) + (B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C) * (B%C))%C);
+
 	}
+
 }
