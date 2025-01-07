bccf310cb436ccd25e7e75b768a78d7f402643d5
SunyoungBae <baesaa0304@naver.com>
2025-01-07 14:32:15 +0900

    [Silver II] Title: 조합 0의 개수, Time: 112 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		long countTwo = countFactor(n, 2) - countFactor(m, 2) - countFactor(n - m, 2);
+        long countFive = countFactor(n, 5) - countFactor(m, 5) - countFactor(n - m, 5);
+        
+        System.out.println(Math.min(countTwo, countFive));
+	}
+	public static long countFactor(long n, int factor) {
+        long count = 0;
+        while (n >= factor) {
+            count += n / factor;
+            n /= factor;
+        }
+        return count;
+    }
+
+}
