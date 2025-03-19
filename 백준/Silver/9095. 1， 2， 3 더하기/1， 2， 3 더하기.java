e93b82be79d50953598e9204384702e8cb5cff90
SunyoungBae <baesaa0304@naver.com>
2025-03-19 15:42:19 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14052 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,31 +1,22 @@
 import java.io.*;
-
+import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
-        StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+			
+		while(T-- > 0) {
+			 int N = Integer.parseInt(br.readLine()); 
+			 System.out.println(solution(N));
+		}
 
-        // 테스트 케이스 처리
-        for (int t = 0; t < T; t++) {
-            int N = Integer.parseInt(br.readLine()); 
-            int[] dp = new int[11];
-            dp[1] = 1; // 1을 만드는 방법: 1
-            dp[2] = 2; // 2를 만드는 방법: (1+1), (2)
-            dp[3] = 4; // 3을 만드는 방법: (1+1+1), (1+2), (2+1), (3)
-
-           
-            for (int j = 4; j <= N; j++) {
-                dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
-            }
-
-          
-            sb.append(dp[N]).append("\n");
-        }
-
-        
-        System.out.println(sb.toString());
-    }
+	}
+	
+	public static int solution(int n) {
+		if(n == 0) return 1;
+		if(n < 0) return 0;
+		
+		return solution(n - 1) + solution(n - 2) + solution(n - 3);
+	}
 }
